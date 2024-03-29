87a8189f9bf31935d3f4e773bf93738432b8e723
Reante Marrel Patterson <75579137+reantep@users.noreply.github.com>
2024-03-29 16:34:32 -0700

    pushing to render

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -148,6 +148,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR/'assets'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads', 'images')
 MEDIA_URL = 'uploads/images/'
